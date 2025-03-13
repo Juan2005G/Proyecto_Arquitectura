9b1891ed2d7c2c5af4bbcb5c54ce108eed9728c1
Sala Fontibon 403 <salafon403@cunadm.edu.local>
2025-03-12 20:28:11 -0500

    Signed-off-by: Sala Fontibon 403 <salafon403@cunadm.edu.local>

--- Dominio/Class1.cs
+++ Dominio/Class1.cs
@@ -2,6 +2,13 @@
 {
     public class Class1
     {
+        Usuario temp =new Usuario();
 
+        //temp.Name = "Test";
+
+        public void prueba()
+        {
+            temp.Name = "test";
+        }
     }
 }
---
+++ Dominio/Usuario.cs
@@ -0,0 +1,25 @@
+﻿/// Entidad de la base de datos 
+namespace Dominio
+{
+    /// <summary>
+    /// Clase que contiene las caracteristicas del Usuario en la bd
+    /// </summary>
+    public class Usuario
+    {
+
+        /// <summary>
+        /// Identificador Unico del usuario
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nombre del Usuario
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Correo electronico del Usuario
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
